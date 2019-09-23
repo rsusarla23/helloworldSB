372b6fba05bb6f7c59984321f67870ebcb13c418
c172863 <c172863@IRL104015.hms.hmsy.com>
2019-09-23 13:16:28 -0500

    Committing

---
+++ src/main/java/com/hms/helloworldSB/HelloworldSbApplication.java
@@ -0,0 +1,13 @@
+package com.hms.helloworldSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldSbApplication.class, args);
+	}
+
+}
